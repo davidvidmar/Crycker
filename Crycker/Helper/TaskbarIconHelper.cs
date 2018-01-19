89fe98854f620821720a26e720547392b67b027e
Bruno Logerfo <bclogerfo@hotmail.com>
2018-01-19 09:30:49 -0200

    disposing resources

--- Crycker/Helper/TaskbarIconHelper.cs
+++ Crycker/Helper/TaskbarIconHelper.cs
@@ -37,56 +37,60 @@ namespace Crycker.Helper
                 if (percentChange != 0)
                 {
                     Color priceColor = (percentChange >= 0) ? Color.Green : Color.OrangeRed;
+                    brush.Dispose();
                     brush = new SolidBrush(priceColor);
                 }
             }
 
-            var bitmap = new Bitmap(16, 16);
-            var graphics = Graphics.FromImage(bitmap);
-
-            // graphics.FillRectangle(Brushes.White, new Rectangle(0, 0, 16, 16));
-
-            string line1;
-            string line2;
-
-            if (lastPrice == 0)
+            using (var bitmap = new Bitmap(16, 16))
+            using (var graphics = Graphics.FromImage(bitmap))
             {
-                line1 = "?";
-                line2 = "";
-            } else if (lastPrice < 1)
-            {                
-                var s = lastPrice.ToString("F3").Split(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0]);
-                line1 = s[0];
-                line2 = s[1];
-            } else if (lastPrice > 1000)
-            {
-                var s = lastPrice.ToString("F0");
-                if (s.Length > 5) s = s.Substring(0, 6);
-                line1 = s.Substring(0, s.Length / 2);
-                line2 = s.Substring(s.Length / 2);
+                // graphics.FillRectangle(Brushes.White, new Rectangle(0, 0, 16, 16));
+
+                string line1;
+                string line2;
+
+                if (lastPrice == 0)
+                {
+                    line1 = "?";
+                    line2 = "";
+                }
+                else if (lastPrice < 1)
+                {
+                    var s = lastPrice.ToString("F3").Split(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0]);
+                    line1 = s[0];
+                    line2 = s[1];
+                }
+                else if (lastPrice > 1000)
+                {
+                    var s = lastPrice.ToString("F0");
+                    if (s.Length > 5) s = s.Substring(0, 6);
+                    line1 = s.Substring(0, s.Length / 2);
+                    line2 = s.Substring(s.Length / 2);
+                }
+                else
+                {
+                    var s = lastPrice.ToString("F2").Split(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0]);
+                    line1 = s[0];
+                    line2 = s[1];
+                }
+
+                var m = graphics.MeasureString(line1, font);
+                var w2 = (int)Math.Round(m.Width / 2);
+                var h2 = (int)Math.Round(m.Height / 2);
+
+                graphics.DrawString(line1, font, brush, bitmap.Size.Width / 2 - w2, -4 + bitmap.Size.Height / 2 - h2);
+
+                m = graphics.MeasureString(line2, font);
+                w2 = (int)Math.Round(m.Width / 2);
+                h2 = (int)Math.Round(m.Height / 2);
+                graphics.DrawString(line2, font, brush, bitmap.Size.Width / 2 - w2, 5 + bitmap.Size.Height / 2 - h2);
+
+                var hIcon = bitmap.GetHicon();
+                var icon = Icon.FromHandle(hIcon);
+                notifyIcon.Icon = icon;
             }
-            else
-            {
-                var s = lastPrice.ToString("F2").Split(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0]);
-                line1 = s[0];
-                line2 = s[1];
-            }
-
-            var m = graphics.MeasureString(line1, font);
-            var w2 = (int)Math.Round(m.Width / 2);
-            var h2 = (int)Math.Round(m.Height / 2);
-
-            graphics.DrawString(line1, font, brush, bitmap.Size.Width / 2 - w2, -4 + bitmap.Size.Height / 2 - h2);
-
-            m = graphics.MeasureString(line2, font);
-            w2 = (int)Math.Round(m.Width / 2);
-            h2 = (int)Math.Round(m.Height / 2);
-            graphics.DrawString(line2, font, brush, bitmap.Size.Width / 2 - w2, 5 + bitmap.Size.Height / 2 - h2);
-
-            var hIcon = bitmap.GetHicon();
-            var icon = Icon.FromHandle(hIcon);            
-            notifyIcon.Icon = icon;
-
+            brush.Dispose();
             notifyIcon.Text = $"{provider} {coin}/{currency}: {lastPrice} ({percentChange:N2}%) @ {lastUpdated.ToLongTimeString()}";
         }        
     }
